643b504fa3e9ee9b08261a6da6b370e7f83b809d
Mingi Kim <zxc12250@naver.com>
2025-12-10 15:07:10 +0900

    commit check

--- src/main/java/com/techup/spring/spring_be/controller/HealthController.java
+++ src/main/java/com/techup/spring/spring_be/controller/HealthController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping("/api/health")
     public String health() {
-        return "OK!!!!!!!!!!!!!!!!!!!!!!";
+        return "OK!!!!!!!!!!!!!!!!!!!!!!!";
     }
 }
